35596b5f9c3a0f64f5f695cf71c9e791e2107cc4
Mohammed Nassar <mohammednassar740@gmail.com>
2025-02-19 09:08:37 +0200

    upload initial files

---
+++ src/AppRouter.tsx
@@ -0,0 +1,9 @@
+function AppRouter() {
+  return (
+    <h1 className="text-3xl font-bold underline text-center mt-80">
+      Welcome to Web Master Workshop E-Commerce React App
+    </h1>
+  );
+}
+
+export default AppRouter;
